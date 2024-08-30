96e8b217ec3d259a519340ba271231c171a28824
myathway-lab <myathway.lab@gmail.com>
2024-08-30 17:48:09 +0000

    updated the tf codes

--- main.tf
+++ main.tf
@@ -6,8 +6,8 @@ resource "hcp_hvn" "mt-hcp-hvn" {
 }
 
 resource "hcp_vault_cluster" "hcp_vault_cluster" {
-  hvn_id          = var.hvn_id
-  cluster_id      = var.cluster_id
+  hvn_id          = hcp_hvn.mt-hcp-hvn.hvn_id
+  cluster_id      = hcp_vault_cluster
   tier            = var.tier
   public_endpoint = true
 }
