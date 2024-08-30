19c0613d0244726543555718e1fb3f911352de49
myathway-lab <myathway.lab@gmail.com>
2024-08-30 13:58:15 +0000

    updated app

--- versions.tf
+++ versions.tf
@@ -1,37 +1,13 @@
 terraform {
   required_providers {
-    vault = {
-      source  = "hashicorp/vault"
-      version = "4.4.0"
-    }
-
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.65.0"
+    hcp = {
+      source = "hashicorp/hcp"
+      version = "0.81.0"
     }
   }
 }
 
-provider "vault" {
-  address = "http://0.0.0.0:8200"
-  token = var.root_token
-}
-
-provider "aws" {
-  region     = "ap-southeast-1"
-  access_key = data.vault_aws_static_access_credentials.creds.access_key
-  secret_key = data.vault_aws_static_access_credentials.creds.secret_key
-  alias      = "singapore-vpc"
-}
-
-provider "aws" {
-  region     = "ap-northeast-1"
-  access_key = data.vault_aws_static_access_credentials.creds.access_key
-  secret_key = data.vault_aws_static_access_credentials.creds.secret_key
-  alias      = "japan-vpc"
-}
-
-data "vault_aws_static_access_credentials" "creds" {
-  backend = "aws-vault-admin-account"
-  name    = "vpc-static-role"
+provider "hcp" {
+  client_id     = "aiJSJsQ00vKlpfdpD141WPRVkjTMD1rX"
+  client_secret = "Auj89H-NRpbAnd_0UndbO17a6c4Prsfor150VSFGJ4mAPh-RkjWpUQnb6daqd6zl"
 }
